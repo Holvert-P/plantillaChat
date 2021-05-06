d4a9c2d9a862596697c3a4f112102ab1e1dd9de2
Holvert <perezholvert@gmail.com>
2021-05-05 18:38:10 -0600

    nada

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+  const button = document.querySelector('.button') 
+  const tooltip = document.querySelector('.tooltip') 
+
+  Popper.createPopper(button, tooltip)
+  
+  
+  function toggle() { 
+    tooltip.classList.toggle('shown')
+  }
+  
+  button.addEventListener('click',function(){
+    toggle()
+  })
